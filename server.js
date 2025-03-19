53b5dcacf1d8f47061a8d17341810e0620b063d4
Alexayul <alexayul05@gmail.com>
2025-03-19 08:04:56 -0400

    Desarrollo del index y arreglo del server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Servir archivos estáticos desde 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
