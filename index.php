1ad13b740b05a5232a73364cabfb30cc35624572
mauriciomarces <mauriciomarces1@gmail.com>
2025-04-02 18:34:48 -0400

    Logo de la empresa en la pestaña de index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<php?>
+<?php
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>JCAutomotors - Inicio</title>
+    <link rel="icon" type="image/png" href="public/logopestania.png">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
